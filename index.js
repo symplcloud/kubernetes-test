323460752f5512c03b577b7091c558f99210b52f
Sean Lee <seanlee@sympl.cloud>
2020-02-04 13:59:39 -0500

    version 2

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('1.0'))
+app.get('/', (req, res) => res.send('2.0'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
